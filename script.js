fe2303a370a4a341eac3d37822a4a7b12494595d
ismaelapan <ismael.apan@gmail.com>
2026-02-05 22:01:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+let teds = 0
+let tedsPerClick = 1
+let tedsPerSecond = 0
+let multiplier = 1  
+let multicounter = 1
+let superPerSecond = 0
+let m = 0
+let k = 0
+let s = 0
+let st = 0
+let pris = 100
+let pris2 = 1500
+let pris3 = 50
+let pris4 = 5000
+ 
+const tedstag = document.getElementById("teds")
+const pristag = document.getElementById("pris")
+const pristag2 = document.getElementById("pris2")
+const pristag3 = document.getElementById("pris3")
+const pristag4 = document.getElementById("pris4")
+const tpc = document.getElementById("tedsPerClick")
+const tps = document.getElementById("tedsPerSecond")
+const mc = document.getElementById("multiplier")
+const multi = document.getElementById("multicounter")
+const sps = document.getElementById("superPerSecond")
+ 
+if (localStorage.getItem("userData")!=undefined){
+    load()
+}
+ 
+function updateUI() {
+  tedstag.textContent = "Total teds: " + Math.floor(teds)
+  pristag.textContent = pris + " teds"
+  pristag2.textContent = pris2 + " teds"
+  pristag3.textContent = pris3 + " teds"
+  pristag4.textContent = pris4 + " teds"
+}
+ 
+function clicked() {
+  teds += tedsPerClick * multiplier
+  updateUI()
+}
+ 
+function upgrade() {
+  if (teds >= pris) {
+    teds -= pris
+    tedsPerClick += 1
+    pris = Math.floor(pris * 1.05)
+    k++
+    tpc.textContent = k
+    updateUI()
+  }
+}
+ 
+function upgrade2() {
+  if (teds >= pris2) {
+    teds -= pris2
+    multiplier *= 1.2    
+    multicounter *= 1.2
+    pris2 = Math.floor(pris2 * 1.5)
+    m++
+    mc.textContent = m
+    multi.textContent = "Multiplier: "+multicounter.toFixed(3)+"x"
+    updateUI()
+  }
+}
+ 
+function upgrade3() {
+  if (teds >= pris3) {
+    teds -= pris3
+    tedsPerSecond += 1
+    pris3 = Math.floor(pris3 * 1.12)
+    s++
+    tps.textContent = s
+    updateUI()
+  }
+}
+ 
+function upgrade4(){
+    if (teds >= pris4) {
+        teds -= pris4
+        superPerSecond += 150
+        pris4 = Math.floor(pris4 * 1.12)
+        st++
+        sps.textContent = st
+        updateUI()
+}
+}
+function load(){
+  let data = localStorage.getItem("userData").split(",")
+  tedsPerClick = +data[0]
+  multiplier = +data[2]
+  tedsPerSecond = +data[1]
+  superPerSecond = +data[3]
+  teds = +data[4]
+}
+ 
+function deletesave(){
+    localStorage.removeItem("userData")
+    window.location.reload()
+}
+ 
+function save(){
+  localStorage.setItem("userData",[tedsPerClick,tedsPerSecond,multiplier,superPerSecond,teds])
+}
+setInterval(() => {
+    teds += tedsPerSecond * multiplier
+    teds += superPerSecond * multiplier
+    updateUI()
+    if (teds >= 1000000) {
+      document.getElementById("ted").src = "gojo.png"
+    }
+    save()
+   
+  }, 1000)
+updateUI()
